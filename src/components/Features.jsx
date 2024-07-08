6320409587848dd5bd52e5baafaf819e275517e1
itzVort3x1 <saikaustubh10@gmail.com>
2024-07-08 19:01:21 +0530

    added animations

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,7 +1,33 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import React from "react";
+import { animateWithGsap } from "../utils/animations";
 
 const Features = () => {
-  return <div>Features</div>;
+  useGSAP(() => {
+    animateWithGsap("#features_title", { y: 0, opacity: 1 });
+  }, []);
+
+  return (
+    <section className="h-full commmon-padding bg-zinc relative overflow-hidden">
+      <div className="screen-max-width">
+        <div className="mb-12 w-full">
+          <h1 id="features_title" className="section-heading">
+            Explore the full story.
+          </h1>
+        </div>
+        <div className="flex flex-col justify-center items-center overflow-hidden">
+          <div className="mt-32 mb-24 pl-24">
+            <h2 className="text-5xl lg:text-7xl font-semibold">iPhone</h2>
+            <h2 className="text-5xl lg:text-7xl font-semibold">
+              Forged in titanium.
+            </h2>
+          </div>
+          <div className="flex-center flex-col sm:px-10"></div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Features;
